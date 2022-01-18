f0d47f42320a0557b88cb5855f0a1b34c359d4b4
EC2 Default User <ec2-user@ip-172-31-5-118.ec2.internal>
2022-01-18 13:15:23 +0000

    devise完了

---
+++ app/models/member.rb
@@ -0,0 +1,16 @@
+class Member < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+         
+  validates :family_name, presence: true
+  validates :first_name, presence: true
+  validates :ruby_family_name, presence: true
+  validates :ruby_first_name, presence: true
+  validates :post_code, presence: true
+  validates :address, presence: true
+  validates :phone_number, presence: true
+  validates :email, presence: true
+  validates :encrypted_password, presence: true
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+<<<<<<< HEAD
 
   devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
   sessions: "admin/sessions"
@@ -9,5 +10,15 @@ Rails.application.routes.draw do
     resources :items, only: [:index, :new, :create, :show, :edit, :update]
   end
 
+=======
+  devise_for :members,skip: [:passwords,], controllers: {
+    registrations: "member/registrations",
+    sessions: 'member/sessions'
+  }
+
+  devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
+    sessions: "admin/sessions"
+  }
+>>>>>>> b04a96d (devise完了)
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
