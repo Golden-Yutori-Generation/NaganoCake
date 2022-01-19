97098e1b51ea7e833d01317646401980439ad02f
EC2 Default User <ec2-user@ip-172-31-0-111.ec2.internal>
2022-01-19 06:23:14 +0000

    conflict修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
 
   devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
   sessions: "admin/sessions"
@@ -10,15 +9,10 @@ Rails.application.routes.draw do
     resources :items, only: [:index, :new, :create, :show, :edit, :update]
   end
 
-=======
   devise_for :members,skip: [:passwords,], controllers: {
     registrations: "member/registrations",
     sessions: 'member/sessions'
   }
 
-  devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
-    sessions: "admin/sessions"
-  }
->>>>>>> b04a96d (devise完了)
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
